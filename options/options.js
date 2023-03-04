df76afbe495d83a6c1f0fd22c610366cfc0df87f
chlauck@gmail.com <chlauck@gmail.com>
2023-03-04 10:41:04 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "calibreak-39224",
   url: "https://calibreak-39224.botics.co",
   api: "https://calibreak-39224.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
